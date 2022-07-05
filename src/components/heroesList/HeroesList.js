f756e72c2681a26aa701a1678d66a2a4a043d214
Eitan <eitanleviberger@gmail.com>
2022-07-05 17:03:31 +0300

    everything is working

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,18 +1,21 @@
 import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
 
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 // Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
+// При клике на "крестик" идет удаление персонажа из общего состояния из heroes[]
 // Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
+// Удаление идет и с json файла при помощи метода DELETE (сделать запрос на тот же адрес локалхост)
+// это есть тут https://www.npmjs.com/package/json-server
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    //const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
@@ -24,6 +27,22 @@ const HeroesList = () => {
 
         // eslint-disable-next-line
     }, []);
+
+    // Функция берет id и по нему удаляет ненужного персонажа из store и из фейк сервер файла JSON
+    // ТОЛЬКО если запрос на удаление прошел успешно
+    // Отслеживайте цепочку действий actions => reducers
+    const onDelete = useCallback((id) => {
+        // Удаление персонажа по его id
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(data => console.log(data, 'Deleted'))
+            .then(dispatch(heroDeleted(id)))
+            .catch(err => console.log(err));
+        // eslint-disable-next-line  
+    }, [request]);
+    // const onDelete = (id) => {
+    //     // Удаление персонажа по его id просто из верстки
+    //     dispatch(heroDeleted(id));
+    // };
 
     if (heroesLoadingStatus === "loading") {
         return <Spinner/>;
@@ -37,11 +56,12 @@ const HeroesList = () => {
         }
 
         return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props}/>
+            return <HeroesListItem key={id} {...props}
+                            onDelete={() => onDelete(id)}/>
         })
     }
 
-    const elements = renderHeroesList(heroes);
+    const elements = renderHeroesList(filteredHeroes);
     return (
         <ul>
             {elements}
